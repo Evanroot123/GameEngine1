e41869d2e2424d2f1620df5a66424d4775885116
Evanroot123 <evanroot3@gmail.com>
2022-05-01 09:52:36 -0500

    window resizing and swap chain recreation

--- VulkanTest/VulkanTutFiles/first_app.h
+++ VulkanTest/VulkanTutFiles/first_app.h
@@ -28,11 +28,14 @@ namespace lve
 		void createPipelineLayout();
 		void createPipeline();
 		void createCommandBuffers();
+		void freeCommandBuffers();
 		void drawFrame();
+		void recreateSwapChain();
+		void recordCommandBuffer(int imageIndex);
 
 		LveWindow lveWindow{ WIDTH, HEIGHT, "SUP BITCHES :)" };
 		LveDevice lveDevice{ lveWindow };
-		LveSwapChain lveSwapChain{ lveDevice, lveWindow.getExtent() };
+		std::unique_ptr<LveSwapChain> lveSwapChain;
 		//LvePipeline lvePipeline{ lveDevice, "Shaders/simple_shader.vert.spv", "Shaders/simple_shader.frag.spv", LvePipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT) };
 		std::unique_ptr<LvePipeline> lvePipeline;
 		VkPipelineLayout pipelineLayout;
--- VulkanTest/VulkanTutFiles/lve_window.h
+++ VulkanTest/VulkanTutFiles/lve_window.h
@@ -17,15 +17,19 @@ namespace lve
 
 		bool shouldClose() { return glfwWindowShouldClose(window); }
 		VkExtent2D getExtent() { return { static_cast<uint32_t>(width), static_cast<uint32_t>(height) }; }
+		bool wasWindowResized() { return framebufferResized; }
+		void resetWindowResizedFlag() { framebufferResized = false; }
 
 		void createWindowSurface(VkInstance instance, VkSurfaceKHR* surface);
 	private:
-		const int width;
-		const int height;
+		int width;
+		int height;
+		bool framebufferResized = false;
 
 		std::string windowName;
 		GLFWwindow* window;
 
 		void initWindow();
+		static void framebufferResizeCallback(GLFWwindow* window, int width, int height);
 	};
 }
